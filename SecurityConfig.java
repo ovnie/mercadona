e22a341b7ef8ad46e6fc3e8068f43312ea925725
Josselin Fatah-Roux <vufic@outlook.com>
2023-09-25 03:06:00 +0200

    Modification pour ajouter Spring Security afin de protéger les pages dans le répertoire admin, ajout dépendance Spring Security dans pom.xml, création d'une classe pour la configuration de la sécurité et modification du format du fichier pour les lignes de commandes de la création de la base

---
+++ SecurityConfig.java
@@ -0,0 +1,61 @@
+package vps9d5e18f7.vps.ovh.net.studi;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        UserDetails adminUser = User.withUsername("admin")
+            .passwordEncoder(password -> passwordEncoder().encode("adminpassword"))
+            .password("adminpassword")
+            .roles("ADMIN")
+            .build();
+
+        UserDetails user = User.withUsername("user")
+            .passwordEncoder(password -> passwordEncoder().encode("userpassword"))
+            .password("userpassword")
+            .roles("USER")
+            .build();
+
+        return new InMemoryUserDetailsManager(adminUser, user);
+    }
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http
+        	.cors() // Activer la configuration CORS
+        	.and()
+            .authorizeRequests(authorizeRequests ->
+                authorizeRequests
+                    .requestMatchers("/admin/**").hasRole("ADMIN")
+                    .requestMatchers("/api/products").permitAll() // Autorisez l'accès à l'API sans authentification
+                    .anyRequest().permitAll()
+            )
+            .formLogin() // Supprimez .loginPage("/login")
+            .and()
+            .logout() // Configure logout behavior here
+            .logoutUrl("/logout") // Spécifiez l'URL de déconnexion personnalisée si nécessaire
+            .logoutSuccessUrl("/") // L'URL où vous souhaitez rediriger après la déconnexion
+            .permitAll(); // Autoriser l'accès à la page de déconnexion
+        
+        http.csrf().disable(); // Désactivez la protection CSRF
+        return http.build();
+    }
+}
